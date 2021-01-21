e45028531a56f45b1d26a9eb0a83bec9f29a5edc
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-21 19:33:01 +0100

    First commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kim on 21.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
